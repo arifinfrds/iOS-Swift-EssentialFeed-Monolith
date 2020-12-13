d751cd61dfb07736bb7b3acfb1b791c7eb19a362
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-12-13 09:59:31 +0700

    Using SUT helper factory method to hide SUT creation complexity ƒor the better future

--- EssentialFeedAppTests/NetworkingModule/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedAppTests/NetworkingModule/Feed API/URLSessionHTTPClientTests.swift
@@ -48,7 +48,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -58,12 +58,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "https://any-url.com")!
         let requestError = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: requestError)
-        
-        let sut = URLSessionHTTPClient()
-        
+                
         let exp = expectation(description: "wait for completion")
         // when
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case .failure(let receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, requestError.domain)
@@ -78,6 +76,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
