61280ff246653b2e6fb1fbb4c652c848f26c3d67
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-12-13 10:22:19 +0700

    Move URL creation using factory helper method

--- EssentialFeedAppTests/NetworkingModule/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedAppTests/NetworkingModule/Feed API/URLSessionHTTPClientTests.swift
@@ -38,8 +38,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
-        
-        let url = URL(string: "https://any-url.com")!
+        let url = makeAnyURL()
         
         let exp = expectation(description: "wait for request")
         URLProtocolStub.observeRequests { request in
@@ -55,13 +54,12 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_failsOnRequestError() {
         // given
-        let url = URL(string: "https://any-url.com")!
         let requestError = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: requestError)
                 
         let exp = expectation(description: "wait for completion")
         // when
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: makeAnyURL()) { result in
             switch result {
             case .failure(let receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, requestError.domain)
@@ -81,6 +79,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(for: sut, file: file, line: line)
         return sut
+    }
+    
+    private func makeAnyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
     
     private class URLProtocolStub: URLProtocol {
